a794833755a92f3fcd908fb3945db809a048510b
Unknown <joshuanits@gmail.com>
2018-06-04 16:33:16 +0930

    Adding everything

---
+++ Flowers/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flowers {
+    public partial class Form1 : Form {
+
+        private Random random;
+
+        public Form1() {
+            InitializeComponent();
+            Activate();
+        }
+
+        private void FormPaint(object sender, PaintEventArgs e) {
+            Graphics g = e.Graphics;
+
+            Pen penWhite = new Pen(Color.GhostWhite, 2);
+            SolidBrush brushBlack = new SolidBrush(Color.Black);
+            SolidBrush brushMagenta = new SolidBrush(Color.FromArgb(200, Color.MediumOrchid));
+
+            random = new Random();
+
+            g.FillRectangle(brushBlack, 0, 0, 1920, 1080);
+
+            List<GraphicsPath> pathList = new List<GraphicsPath>();
+            
+
+
+            for (int i = 0; i < random.Next(10, 20); i++) {
+                GraphicsPath path = GenerateStem(300, 600, -1 * Math.PI / 3 - Math.PI / 2, Math.PI / 3 - Math.PI / 2);
+
+
+                g.DrawPath(penWhite, path);
+                pathList.Add(path);
+            }
+
+            for(int i = 0; i < (random.Next(5, 25)); i++) {
+                GraphicsPath path = pathList[random.Next(pathList.Count)];
+
+                double u = random.NextDouble() * 0.3 + 0.5;
+
+                PointF[] points0 = path.PathPoints;
+                PointF[] points1 = new PointF[3];
+                PointF[] points2 = new PointF[2];
+
+                points1[0] = LerpPoint(points0[0], points0[1], u);
+                points1[1] = LerpPoint(points0[1], points0[2], u);
+                points1[2] = LerpPoint(points0[2], points0[3], u);
+
+                points2[0] = LerpPoint(points1[0], points1[1], u);
+                points2[1] = LerpPoint(points1[1], points1[2], u);
+
+                PointF point = LerpPoint(points2[0], points2[1], u);
+                Pen pen1 = new Pen(Color.FromArgb(0, 0, 255));
+                Pen pen2 = new Pen(Color.FromArgb(255, 0, 0));
+                Pen pen3 = new Pen(Color.FromArgb(0, 255, 0));
+
+                //g.DrawLine(pen1, points0[0], points0[1]);
+                //g.DrawLine(pen1, points0[1], points0[2]);
+                //g.DrawLine(pen1, points0[2], points0[3]);
+                    
+                //g.DrawLine(pen2, points1[0], points1[1]);
+                //g.DrawLine(pen2, points1[1], points1[2]);
+
+                //g.DrawLine(pen3, points2[0], points2[1]);
+
+                GraphicsPath p = GenerateStem(100, 250, -2 * Math.PI / 2, -1 * Math.PI / 2, Point.Round(point));
+                pathList.Add(p);
+                g.DrawPath(penWhite, p);
+            }
+            
+            for(int i = 0; i < 5000; i++) {
+                GraphicsPath path = pathList[random.Next(pathList.Count)];
+                PointF point = path.PathPoints[3];
+
+                double r = random.NextDouble();
+                double d = Math.Pow(14, 1.1 * r + 1) - 30;
+                double a = random.NextDouble() * Math.PI * 2;
+
+                double size = random.NextDouble() * 5 + 1;
+
+                PointF loc = new PointF((float)(point.X + d * Math.Cos(a) - size / 2), (float)(point.Y + d * Math.Sin(a) - size / 2));
+
+                g.FillEllipse(brushMagenta, new RectangleF(loc, new Size((int) size, (int) size)));
+            }
+
+            EventHandler temp = MyEvent;
+            if (temp != null) {
+               temp(this, null);
+            }
+        }
+
+        private GraphicsPath GenerateStem(double minD, double maxD, double minA, double maxA, [Optional] Point offset) {
+            GraphicsPath path = new GraphicsPath();
+
+            double d = random.NextDouble() * (maxD - minD) + minD;
+            double a = random.NextDouble() * (maxA - minA) + minA;
+
+            if(offset.Equals(new Point(0, 0))) offset = new Point(1920 / 2, 1080 / 6 * 5);
+            Point start = new Point(0, 0);
+            start.Offset(offset);
+            Point end = new Point(Convert.ToInt32(Math.Cos(a) * d), Convert.ToInt32(Math.Sin(a) * d));
+            end.Offset(offset);
+
+            Point control = end;
+            control.Offset(random.Next(-50, 50), random.Next(25, 100));
+
+            Point control1 = Point.Round(LerpPoint(start, control, 2 / 3));
+            Point control2 = Point.Round(LerpPoint(control, end, 1 / 3));
+            //g.DrawLine(penWhite, start, end);
+            path.AddBezier(start, control1, control2, end);
+
+            //Pen penDebug = new Pen(Color.FromArgb(0, 255, 0));
+            //Size s20 = new Size(20, 20);
+            //Size s10 = new Size(10, 10);
+            //Size s5 = new Size(5, 5);
+            //g.DrawEllipse(penDebug, new Rectangle(start - s10, s20));
+            //g.DrawEllipse(penDebug, new Rectangle(control - s5, s10));
+            //g.DrawEllipse(penDebug, new Rectangle(end - s10, s20));
+            return path;
+        }
+        
+        private async void MyEvent(object sender, EventArgs e) {
+            await Task.Delay(1000);
+            Refresh();
+        }
+
+        private void OnClick(object sender, EventArgs e) {
+            this.Refresh();
+        }
+
+        float Lerp(float a, float b, double c) {
+            return ((b - a) * (float) c + a);
+        }
+
+        PointF LerpPoint(PointF a, PointF b, double u) {
+            return new PointF(Lerp(a.X, b.X, u), Lerp(a.Y, b.Y, u));
+        }
+    }
+}
---
+++ Spirograph/Form1.cs
@@ -0,0 +1,238 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spirograph {
+    public partial class Form1 : Form {
+        static Random random;
+        GraphicsPath path;
+        GraphicsPath path2;
+        double progress;
+        double radius1;
+        double radius2;
+        double d;
+        Graphics g;
+        double radsPerColour;
+        int colour;
+        Bitmap bitmap;
+
+        EventHandler RedrawEvent;
+
+        Point lastCircle;
+        Point lastDraw;
+
+        Brush brushBlack = new SolidBrush(Color.Black);
+        Brush brushWhite = new SolidBrush(Color.White);
+        Pen penWhite = new Pen(Color.White);
+        Pen penGreen = new Pen(Color.Green);
+        Pen penBlue = new Pen(Color.Blue);
+
+        Pen[] penColours;
+
+        TimeSpan delay;
+
+        public Form1() {
+            InitializeComponent();
+
+            Activate();
+
+            RedrawEvent = Redraw;
+
+            random = new Random();
+            delay = new TimeSpan(10000);
+
+            penColours = new Pen[360];
+            for(int i = 0; i < 360; i++) {
+                penColours[i] = new Pen(HSL2RGB((float) i / 360, 1, 0.5));
+            }
+
+            RedrawEvent?.Invoke(this, null);
+
+            Start(this, null);
+        }
+
+        private void Form_Paint(object sender, PaintEventArgs e) {
+            g = e.Graphics;
+
+            Graphics gr = Graphics.FromImage(bitmap);
+
+            //g.FillRectangle(brushBlack, new Rectangle(0, 0, Width, Height));
+
+            double x = (Math.Cos(progress) * radius1) + 1920 / 2;
+            double y = (Math.Sin(progress) * radius1) + 1080 / 2;
+
+            double offSetX = x + (-Math.Cos(progress) * radius2);
+            double offSetY = y + (-Math.Sin(progress) * radius2);
+
+            Point c2 = new Point((int) offSetX, (int) offSetY);
+            
+            //g.DrawEllipse(penBlue, offSetX - radius2 / 2, offSetY - radius2 / 2, radius2, radius2);
+
+
+            double innerSpin = -(progress) * ((radius1 - radius2) / radius2);
+            offSetX += d * Math.Cos(innerSpin);
+            offSetY += d * Math.Sin(innerSpin);
+
+            Point circlePoint = new Point((int)x, (int) y);
+
+            if (lastCircle != Point.Empty) gr.DrawLine(penWhite, lastCircle, circlePoint);
+
+            lastCircle = circlePoint;
+
+            Point drawPoint = new Point((int)offSetX, (int)offSetY);
+
+            if(lastDraw != Point.Empty) gr.DrawLine(penColours[(int)(progress / radsPerColour) % 360], lastDraw, drawPoint);
+
+            lastDraw = drawPoint;
+
+            //g.DrawPath(penWhite, path);
+            //g.DrawPath(penColours[colour % 360], path2);
+
+            g.DrawImage(bitmap, 0, 0);
+
+            DrawEllipse(penBlue, c2, (float)radius2);
+            g.DrawLine(penBlue, c2, new Point((int)offSetX, (int)offSetY));
+            g.DrawString(progress.ToString(), SystemFonts.DefaultFont, brushWhite, new Point(50, 50));
+
+            progress += Math.PI / 100;
+            //ogress += 0.001f;
+
+            if (progress % radsPerColour <= Math.PI / 99) {
+                colour++;
+            }
+
+            
+            RedrawEvent?.Invoke(this, null);
+        }
+
+        private void Start(object sender, EventArgs e) {
+            path = new GraphicsPath();
+            path2 = new GraphicsPath();
+            progress = 0;
+            colour = 0;
+
+            lastCircle = Point.Empty;
+            lastDraw = Point.Empty;
+
+            radius1 = random.Next(100, 400);
+
+            while(radius2 == 0) radius2 = random.Next(-100, 100);
+
+            //radius1 = random.Next(180, 220);
+            d = random.Next(-100, -100);
+
+            bitmap = new Bitmap(1920, 1080);
+
+            Graphics.FromImage(bitmap).Clear(Color.Black);
+
+            int hcf = 1;
+            for(int i = 1; i <= radius1; i++) {
+                if (radius1 % i == 0 && radius2 % i == 0) hcf = i;
+            }
+
+            Debug.WriteLine(hcf);
+            Debug.WriteLine(radius2 / hcf);
+
+            int nodes = (int) radius1 / hcf;
+            double radsPerNode = Math.Abs((Math.PI * 2 * (radius2 / radius1)));
+            double coloursPerNode = 360 / nodes;
+
+            radsPerColour = radsPerNode / coloursPerNode;
+
+            
+
+            //radsPerNode = 0.1;
+        }
+
+        private async void Redraw(object sender, EventArgs e) {
+            await Task.Delay(delay);
+            Refresh();
+        }
+
+        void DrawEllipse(Pen p, Point c, float r) {
+            g.DrawEllipse(p, c.X - r, c.Y - r, r * 2, r * 2);
+        }
+
+        public Color HSL2RGB(double h, double sl, double l) {
+
+            double v;
+            double r, g, b;
+
+            r = l;   // default to 
+            g = l;
+            b = l;
+
+            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
+
+            if (v > 0) {
+                double m;
+                double sv;
+                int sextant;
+                double fract, vsf, mid1, mid2;
+
+                m = l + l - v;
+                sv = (v - m) / v;
+                h *= 6.0;
+                sextant = (int)h;
+                fract = h - sextant;
+                vsf = v * sv * fract;
+                mid1 = m + vsf;
+                mid2 = v - vsf;
+
+                switch (sextant) {
+                    case 0:
+                        r = v;
+                        g = mid1;
+                        b = m;
+                        break;
+
+                    case 1:
+                        r = mid2;
+                        g = v;
+                        b = m;
+                        break;
+
+                    case 2:
+                        r = m;
+                        g = v;
+                        b = mid1;
+                        break;
+
+                    case 3:
+                        r = m;
+                        g = mid2;
+                        b = v;
+
+                        break;
+                    case 4:
+                        r = mid1;
+                        g = m;
+                        b = v;
+
+                        break;
+                    case 5:
+                        r = v;
+                        g = m;
+                        b = mid2;
+
+                        break;
+                }
+            }
+
+            Color rgb = Color.FromArgb((int) (r * 255), (int) (g * 255), (int) (b * 255));
+
+            return rgb;
+
+        }
+    }
+
+
+}
